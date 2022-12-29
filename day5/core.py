36cd52a6ef6e3ea7c160f7f591c7a33f7ec09467
Philippe Roland <roland.phil@gmail.com>
2022-12-29 23:31:47 +0100

    Day1 Part 1 complete

--- day5/core.py
+++ day5/core.py
@@ -1,28 +1,36 @@
 import os
+from functools import reduce
+
 from utils import read_file, print_lines
+
 
 
 def solution(lines):
     print_lines(lines)
     setup, instructions = parse_input(lines)
     print(setup)
+    run_instructions(setup, instructions)
+    result_map = map((lambda stack: stack[0] if stack else ''), setup)
+    result = reduce((lambda crate1, crate2: crate1+crate2), result_map)
+    return result, setup
 
 def parse_input(lines):
     #subdivision of lines containing the lines with the stacks formation, minus the column labels at the bottom
     initial_stack_lines = []
     instructions_lines = []
     reading_instructions = False
-    #list of lists
+    #list of lists, orderered left to right and top to bottom
     stacks = []
     stacks_width = 0
+    #list of lists, each entry having From, To, Amount in 0, 1, 2 respectively
+    instructions = []
 
     for line in lines:
-        if line[-1].isdigit():
+        if not reading_instructions and line[-1].isdigit():
             stacks_width = int(line[-1])
             reading_instructions = True
-            break
         else:
-            initial_stack_lines.append(line)
+            instructions_lines.append(line) if reading_instructions else initial_stack_lines.append(line)
 
     for i in range(0, stacks_width):
         stacks.append([])
@@ -36,11 +44,24 @@ def parse_input(lines):
                 crate_index = i//4
                 stacks[crate_index].append(line[i])
 
-    return stacks, False
+    for line in instructions_lines:
+        if line:
+            instruction_numbers = list(filter(lambda txt: txt.isnumeric(), line.split(' ')))
+            #sample instruction: "move 14 from 2 to 3"
+            #turns into [2,3,14]
+            instruction = [instruction_numbers[1], instruction_numbers[2], instruction_numbers[0]]
+            instructions.append(instruction)
 
+    return stacks, instructions
 
-
-
+def run_instructions(setup, instructions):
+    for instruction in instructions:
+        from_val = int(instruction[0])-1
+        to_val = int(instruction[1])-1
+        amount = int(instruction[2])
+        for i in range(1, amount+1):
+            crate = setup[from_val].pop(0)
+            setup[to_val].insert(0, crate)
 
 
 if __name__ == '__main__':
@@ -50,7 +71,7 @@ if __name__ == '__main__':
 
 
 
-    solution = solution(lines)
+    solution, setup = solution(lines)
     print(solution)
     print('----------------------------------------------------------\n'
           '----------------------------------------------------------')
--- day5/test.py
+++ day5/test.py
@@ -5,7 +5,7 @@ from utils import read_file, print_lines
 
 class Day5Test(unittest.TestCase):
 
-    def test_parse_input(self):
+    def test_parse_input_stacks(self):
         lines = read_file('test_input')
         print_lines(lines)
         setup, instructions = core.parse_input(lines)
@@ -13,9 +13,22 @@ class Day5Test(unittest.TestCase):
         self.assertEqual(3, len(setup[1]))
         self.assertEqual('D', setup[1][0])
 
-    #def test_solution(self):
-        #lines = read_file('test_input')
-        #self.assertEqual(42, core.solution(lines))
+    def test_parse_input_instructions(self):
+        lines = read_file('test_input')
+        print_lines(lines)
+        setup, instructions = core.parse_input(lines)
+        self.assertEqual(4, len(instructions))
+        self.assertEqual('2', instructions[0][0]) #From
+        self.assertEqual('1', instructions[0][1]) #To
+        self.assertEqual('1', instructions[0][2]) #Quantity
+
+    def test_solution(self):
+        lines = read_file('test_input')
+        solution, final_setup = core.solution(lines)
+        self.assertEqual('CMZ', solution)
+        self.assertEqual('N', final_setup[2][1])
+        self.assertEqual('D', final_setup[2][2])
+        self.assertEqual('P', final_setup[2][3])
 
 if __name__ == '__main__':
     unittest.main()
