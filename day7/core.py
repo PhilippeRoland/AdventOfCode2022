7a9b7457c39e5e066c02fbed32fbb012d192c675
Philippe Roland <roland.phil@gmail.com>
2023-01-26 00:26:14 +0100

    Started parsing code and tree data structure

--- day7/core.py
+++ day7/core.py
@@ -1,8 +1,39 @@
 import os
 from utils import read_file, print_lines
 
+
 def solution(lines):
     print_lines(lines)
+    # these inputs always start with $ cd /
+    current_dir = Resource("/", 0, None)
+    listing_in_progress = False
+    for line in lines:
+        if line.startswith("$ cd "):
+            new_dir = line.replace("$ cd ", "")
+            if "/" == new_dir:
+                continue
+            if ".." == new_dir:
+                current_dir = current_dir.parent
+            else:
+                current_dir = current_dir.find_child(new_dir)
+
+class Resource:
+    def __init__(self, name, size, parent):
+        # size can be 0, this means it's a dir
+        self.name = name
+        self.size = size
+        self.parent = parent
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def find_child(self, name):
+        #this assumes there is such a child, and would fail otherwise
+        next(filter(lambda child: child.name == name, self.children))
+
+    def calc_size(self):
+        return sum(map(lambda child: child.calc_size(), self.children)) + self.size
 
 
 if __name__ == '__main__':
--- day7/test.py
+++ day7/test.py
@@ -2,10 +2,17 @@ import unittest
 import core
 from utils import read_file
 
+
 class Day7Test(unittest.TestCase):
+
+    def test_calc_size(self):
+        lines = read_file('test_input_1')
+        self.assertEqual(2000, core.solution(lines))
+
     def test_solution(self):
         lines = read_file('test_input')
         self.assertEqual(95437, core.solution(lines))
+
 
 if __name__ == '__main__':
     unittest.main()
